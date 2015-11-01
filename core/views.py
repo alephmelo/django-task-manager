ffdbc2ced2934aa46511fad2c2c273a7b8b3ac80
Aleph Melo <alephmelo@icloud.com>
2015-11-01 15:57:38 -0300

    Fix dashboard, yay

--- core/views.py
+++ core/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 from .models import Task
 
 def home(request):
-    return render(request, 'home.html')
+    if request.user.is_authenticated():
+        return dashboard(request)
+    else:
+        return render(request, 'home.html')
 
 def contact(request):
     return render(request, 'contact.html')
 
 def dashboard(request):
 	results = Task.objects.all()
-	return render(request, 'home.html', {'results': results})
+	return render(request, 'dashboard.html', {'results': results})
